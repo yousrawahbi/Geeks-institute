befbb619f81f69a9af1e74ef21e46cf2fb5b2380
yousrawahbi <yosrawahbii@gmail.com>
2025-08-12 02:13:13 +0200

    updt

---
+++ week1/day1/Exercice XP GOLD 3.py
@@ -0,0 +1,7 @@
+my_name= "yousra"
+user_name=(input("what's your name ?"))
+while user_name.lower !=my_name.lower :
+    print("Hi "+user_name + " it's not my name")
+    user_name=(input("try another name : "))
+print("it's my name !!")
+
