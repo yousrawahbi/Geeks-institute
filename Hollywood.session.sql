cfd18735ec1f60b2ba3c205528fe95634cb2a8a7
yousrawahbi <yosrawahbii@gmail.com>
2025-08-20 12:28:18 +0200

    dailychal

--- Hollywood.session.sql
+++ Hollywood.session.sql
@@ -1,7 +1,64 @@
-CREATE TABLE actors (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50)
+CREATE TABLE actors(
+ actor_id SERIAL PRIMARY KEY,
+ first_name VARCHAR (50) NOT NULL,
+ last_name VARCHAR (100) NOT NULL,
+ age DATE NOT NULL,
+ number_oscars SMALLINT NOT NULL
 )
 
-INSERT INTO actors
-VALUES ()
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Matt','Damon','08/10/1970', 5);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('George','Clooney','06/05/1961', 2);
+
+SELECT * FROM actors;
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Lana','Queen','16/08/1987', 4);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Hanna','Jimm','24/11/1994', 6);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Sam','Good','10/10/1977', 1),('Max','Moon','16/08/1996', 3),('Joe','More','11/11/2003', 1);
+
+SELECT * FROM actors WHERE first_name = 'Matt';
+
+SELECT * FROM actors WHERE number_oscars >= 3;
+
+SELECT last_name FROM actors WHERE first_name != 'Matt' ;
+
+SELECT first_name, last_name FROM actors WHERE first_name = 'Matt'  AND 
+last_name = 'Damon' ;
+
+SELECT first_name, last_name FROM actors WHERE first_name = 'Matt'  AND 
+last_name = 'Clooney' ;
+
+SELECT first_name, last_name FROM actors WHERE first_name = 'Matt'  OR  
+number_oscars = 2 ;
+
+SELECT * FROM actors WHERE last_name LIKE '%mon%';
+
+SELECT first_name FROM actors WHERE last_name LIKE '%y'
+SELECT first_name FROM actors WHERE last_name LIKE 'da%'
+SELECT first_name FROM actors WHERE last_name ILIKE 'da%'
+SELECT * FROM actors LIMIT 1;
+SELECT * FROM actors WHERE age > '1960-01-01' LIMIT 1;
+SELECT * FROM actors WHERE age > '1960-01-01' LIMIT 3 OFFSET 2;
+SELECT * FROM actors WHERE age > '1960-01-01' ORDER BY first_name ASC
+
+UPDATE actors 
+SET age='1970-01-01' 
+WHERE first_name='Matt' AND last_name='Damon'
+RETURNING 
+    actor_id,
+    first_name, 
+    last_name,
+    age;
+
+DELETE FROM actors WHERE actor_id=4
+RETURNING actor_id, first_name, last_name, number_oscars;
+
+
+ SELECT COUNT(*) AS actor_count FROM actors;
--- week2/day1/daily_challenge.sql
+++ week2/day1/daily_challenge.sql
@@ -1,2 +1,34 @@
-CREATE DATABASE bootcamp
-CREATE TABLE students
+CREATE TABLE actors(
+ actor_id SERIAL PRIMARY KEY,
+ first_name VARCHAR (50) NOT NULL,
+ last_name VARCHAR (100) NOT NULL,
+ age DATE NOT NULL,
+ number_oscars SMALLINT NOT NULL
+)
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Matt','Damon','08/10/1970', 5);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('George','Clooney','06/05/1961', 2);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Lana','Queen','16/08/1987', 4);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Hanna','Jimm','24/11/1994', 6);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Sam','Good','10/10/1977', 1),('Max','Moon','16/08/1996', 3),('Joe','More','11/11/2003', 1);
+
+SELECT * FROM actors
+ --qst1 Count how many actors are in the table
+
+SELECT COUNT(*) AS actor_count FROM actors;
+
+ --qst2 Try to add a new actor with some blank fields. 
+ --What do you think the outcome will be ?
+INSERT INTO actors (first_name, last_name, age, number_oscars) 
+VALUES (NULL, NULL, 30, 1);
+
+-- Erreur, car on a déclaré first_name et last_name NOT NULL donc on doit les remplir
