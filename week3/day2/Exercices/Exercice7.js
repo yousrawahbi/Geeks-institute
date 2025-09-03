15658442af4678f2354a582c4678e32faf32a883
yousrawahbi <yosrawahbii@gmail.com>
2025-09-03 07:55:41 +0200

    ex

---
+++ week3/day2/Exercices/Exercice7.js
@@ -0,0 +1,15 @@
+// qst2
+const allBooks = [
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        image: "https://images.pexels.com/photos/1926988/pexels-photo-1926988.jpeg",
+        alreadyRead: true
+    },
+    {
+        title: "Harry Potter",
+        author: "J.K. Rowling",
+        image: "https://images.pexels.com/photos/14965132/pexels-photo-14965132.jpeg",
+        alreadyRead: false
+    }
+];
