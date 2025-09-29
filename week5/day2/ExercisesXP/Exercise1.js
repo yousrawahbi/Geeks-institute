a82de503f52481ca70c8182f97c4c3212d7efed2
yousrawahbi <yosrawahbii@gmail.com>
2025-09-29 12:18:48 +0200

    ex1

---
+++ week5/day2/ExercisesXP/Exercise1.js
@@ -0,0 +1,13 @@
+fetch('https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
